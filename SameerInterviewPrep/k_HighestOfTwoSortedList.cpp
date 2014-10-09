1192af7eca37a4e0d0939466118799fc00d59e3b
SameerSawla <sameer.sawla.cs@gmail.com>
2014-10-09 08:31:03 -0400

    k highest sums from two sorted lists

---
+++ SameerInterviewPrep/k_HighestOfTwoSortedList.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+#include<vector>
+#include<cassert>
+using namespace std;
+
+void calculate_k_highest(vector<int> &sorted1,vector<int> &sorted2,int k)
+{
+
+ 		
+ 	assert(k <= sorted1.size() + sorted2.size());
+ 	int i = sorted1.size()-1;
+ 	int j = sorted2.size()-1;
+ 	unsigned sorted2DiffPartTwo = 0;
+ 	unsigned sorted1DiffPartTwo = 0;
+ 	cout<<"\n";
+ 	for(unsigned int h=1;h<=k;h++)
+ 	{
+ 		cout<<h<<". "<<sorted1[i]+sorted2[j]<<"\n";
+ 		if(i!=0)
+ 		{
+ 			sorted1DiffPartTwo = sorted1[i-1];
+ 		}
+ 		
+ 		if(j!=0)
+ 		{
+ 			sorted2DiffPartTwo = sorted2[j-1];	
+ 		}
+
+ 		int sorted1Diff = sorted1[i] - sorted1DiffPartTwo;
+ 		int sorted2Diff = sorted2[j] - sorted2DiffPartTwo;
+ 		if(sorted1Diff<sorted2Diff)
+ 		{
+ 			if(i!=0)
+ 			{
+ 			 i--;
+ 			}
+ 			else
+ 			{
+ 			 j--;
+ 			}
+ 		}
+ 		else
+ 		{
+ 			if(j!=0)
+ 			{
+ 			 j--;
+ 			}
+ 			else
+ 			{
+ 			 i--;	
+ 			}
+ 		}
+ 	}	
+ 	cout<<"\n "<<i<<" "<<j<<"\n";
+}
+
+int main()
+{
+	vector<int> sorted1;
+	vector<int> sorted2;
+
+	sorted1.push_back(1);
+	sorted1.push_back(2);
+	sorted1.push_back(3);
+	sorted1.push_back(4);
+
+	sorted2.push_back(5);
+	sorted2.push_back(6);
+	sorted2.push_back(7);
+	sorted2.push_back(8);	
+
+	calculate_k_highest(sorted1,sorted2,8);
+	return 0;
+}
